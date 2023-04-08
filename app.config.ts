83f5641f5d3ddf8ec795534c8563fc4572830279
oeyoews <jyao4783@gmail.com>
2023-04-08 14:16:06 +0800

    init: config for personal

--- app.config.ts
+++ app.config.ts
@@ -1,41 +1,41 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: "Blog",
+    description: "Personal blog",
     image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      src: "/social-card-preview.png",
+      alt: "An image showcasing my project.",
       width: 400,
-      height: 300
+      height: 300,
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: "left", // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
-      }
+        path: "/logo.svg", // path of the logo
+        pathDark: "/logo-dark.svg", // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: "alpine", // alt of the logo
+      },
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        enabled: false, // possible value are : true | false
+        repository: "https://www.github.com/nuxt-themes/alpine", // our github repository
       },
-      navigation: true, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      navigation: false, // possible value are : true | false
+      alignment: "center", // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: "", // string that will be displayed in the footer (leave empty or delete to disable)
     },
-    socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+    /* socials: {
+      twitter: "nuxtlabs",
+      instagram: "atinuxt",
       linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
-    },
+        icon: "uil:linkedin",
+        label: "LinkedIn",
+        href: "https://www.linkedin.com/company/nuxtlabs",
+      },
+    }, */
     form: {
-      successMessage: 'Message sent. Thank you!'
-    }
-  }
-})
+      successMessage: "Message sent. Thank you!",
+    },
+  },
+});
